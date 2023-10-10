bc770d04ce342b77573a5c11b85403b39cf47ad9
Rund Tawfiq <107937921+rund0wn@users.noreply.github.com>
2023-10-10 10:52:20 +0300

    Create fetch_data.sh
    Download raw reads from ENA for 2 distinct mock communities

---
+++ fetch_data.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#PRJNA595174 data
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/033/SRR10688033/SRR10688033_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/032/SRR10688032/SRR10688032_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/014/SRR10688014/SRR10688014_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/036/SRR10688036/SRR10688036_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/002/SRR10688002/SRR10688002_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/005/SRR10688005/SRR10688005_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/006/SRR10688006/SRR10688006_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/004/SRR10688004/SRR10688004_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/035/SRR10688035/SRR10688035_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/006/SRR10688006/SRR10688006_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/035/SRR10688035/SRR10688035_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/032/SRR10688032/SRR10688032_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/031/SRR10688031/SRR10688031_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/034/SRR10688034/SRR10688034_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/003/SRR10688003/SRR10688003_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/001/SRR10688001/SRR10688001_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/004/SRR10688004/SRR10688004_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/034/SRR10688034/SRR10688034_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/033/SRR10688033/SRR10688033_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/014/SRR10688014/SRR10688014_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/025/SRR10688025/SRR10688025_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/031/SRR10688031/SRR10688031_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/002/SRR10688002/SRR10688002_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/037/SRR10688037/SRR10688037_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/025/SRR10688025/SRR10688025_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/003/SRR10688003/SRR10688003_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/037/SRR10688037/SRR10688037_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/005/SRR10688005/SRR10688005_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/001/SRR10688001/SRR10688001_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR106/036/SRR10688036/SRR10688036_1.fastq.gz
+
+#PRJNA659641 data
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/087/SRR12538887/SRR12538887_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/086/SRR12538886/SRR12538886_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/085/SRR12538885/SRR12538885_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/084/SRR12538884/SRR12538884_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/083/SRR12538883/SRR12538883_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/086/SRR12538886/SRR12538886_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/087/SRR12538887/SRR12538887_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/085/SRR12538885/SRR12538885_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/084/SRR12538884/SRR12538884_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/083/SRR12538883/SRR12538883_1.fastq.gz
